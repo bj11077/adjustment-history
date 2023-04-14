c6580cca7112973a39ef653ab179f595db551a66
bjp <bj11077@gmail.com>
2023-04-14 17:42:45 +0900

    로그인 권한 TODO완료 / 주석삭제

--- src/main/java/nn/trade/user/UserController.java
+++ src/main/java/nn/trade/user/UserController.java
@@ -10,11 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/user")
 public class UserController {
 
-    /**
-     * @TODO
-     * 1. 로그인 - 권한분배 필요
-     * 2. 시큐리티적용
-     */
+
 
     private final UserService service;
 
