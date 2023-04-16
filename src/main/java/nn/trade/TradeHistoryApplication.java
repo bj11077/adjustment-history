9a486ed4fcf78159497cc1dfcdc5f6f1025e48db
bjune-park <54969248+bj11077@users.noreply.github.com>
2023-04-16 20:30:05 +0900

    현재 상속구조 past 브랜치로이동

--- src/main/java/nn/trade/TradeHistoryApplication.java
+++ src/main/java/nn/trade/TradeHistoryApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * @TODO
+ * CommonService 및 CommonController 상속제거 ( past 브랜치로 이동 )
+ * 1. 응답용DTO , 요청용DTO 구분이 힘들어짐
+ * 2. Swagger사용시 각 요청에 대해 상세한 설명하기가 힘듬
+ */
+
 @EnableJpaRepositories
 @SpringBootApplication
 public class TradeHistoryApplication {
